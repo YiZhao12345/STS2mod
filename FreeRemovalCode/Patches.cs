4a75d7914f035c8c0d3bdf67993baaa92fe97e87
YiZ <253130973@wqq.com>
2026-04-23 09:43:35 +0800

    已经完成0元删卡功能，开始借贷功能

---
+++ FreeRemovalCode/Patches.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Merchant;
+
+namespace FreeRemoval.FreeRemovalCode.Patches;
+
+[HarmonyPatch(typeof(MerchantCardRemovalEntry), nameof(MerchantCardRemovalEntry.CalcCost))]
+public static class FreeRemovalPatch
+{
+    [HarmonyPostfix]
+    public static void Postfix(MerchantCardRemovalEntry __instance)
+    {
+        __instance._cost = 0;
+    }
+}
